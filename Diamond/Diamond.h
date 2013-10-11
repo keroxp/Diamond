0b575eb57a218c645c74d43bed1feb91d7b1087e
Yusuke Sakurai <kerokerokerop@gmail.com>
2013-10-11 10:57:43 +0900

    Initial Commit

---
+++ Diamond/Diamond.h
@@ -0,0 +1,13 @@
+//
+//  Diamond.h
+//  Diamond
+//
+//  Created by 桜井雄介 on 2013/10/11.
+//  Copyright (c) 2013年 Yusuke Sakurai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Diamond : NSObject
+
+@end
---
+++ Diamond/Diamond.m
@@ -0,0 +1,13 @@
+//
+//  Diamond.m
+//  Diamond
+//
+//  Created by 桜井雄介 on 2013/10/11.
+//  Copyright (c) 2013年 Yusuke Sakurai. All rights reserved.
+//
+
+#import "Diamond.h"
+
+@implementation Diamond
+
+@end
